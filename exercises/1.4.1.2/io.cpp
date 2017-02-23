b00c7eb9337f8e8177540721ece34cc1d8dd3118
Patrick Trentin <patrick.trentin.88@gmail.com>
2017-02-23 20:32:00 +0100

    1.4.1.2

---
+++ exercises/1.4.1.2/io.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    const int THISYEAR = 2017;
+    std::string yourName;
+    int birthYear;
+
+    std::cout << "What is your name? " << std::flush;
+    std::cin >> yourName;
+
+    std::cout << "What year were you born? ";
+    std::cin >> birthYear;
+
+    std::cout << "Your name is " << yourName
+              << " and you are approximately "
+              << (THISYEAR - birthYear)
+              << " years old." << std::endl;
+}
